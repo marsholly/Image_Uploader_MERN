7adb5590ebce9f9b61f581fa6f807c15a39ff4b0
HONG ZHOU <holly_zhou@yahoo.com>
2016-11-21 10:13:33 -0800

    get all images

--- src/components/AllImage.js
+++ src/components/AllImage.js
@@ -5,19 +5,49 @@ import ImageAction from '../actions/ImageAction';
 
 
 export default class AllImage extends Component {
-  // constructor(){
-  //   super();
-  //   this.submitFile = this.submitFile.bind(this);
-  // }
-  //
-  // submitFile(file){
-  //   // ImageAction.createImage(file);
-  // }
+  constructor(props){
+    super(props);
+
+    this.state = {
+      images: ImageStore.getAll(),
+    }
+
+    this._onChange = this._onChange.bind(this);
+  }
+
+  componentWillMount() {
+    ImageAction.getAllImages();
+    ImageStore.startListening(this._onChange);
+  }
+
+  componentWillUnmount() {
+    ImageStore.stopListening(this._onChange);
+  }
+
+  _onChange() {
+    this.setState({
+      images: ImageStore.getAll()
+    });
+  }
 
   render() {
+    let {images} = this.state;
+    let listImages;
+    if(images) {
+      listImages = images.map(image => {
+        return (
+          <ImageList key={image._id} {...image} />
+        )
+      })
+    } else {
+      listImages = <div></div>;
+    }
+
     return (
       <div className="container">
-        <h1 className="text-center">Image</h1>
+        <div className="row">
+          {listImages}
+        </div>
       </div>
     )
   }
